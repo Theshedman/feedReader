13f531d0807f4acfb07850a3d0d02932266f0a9c
AJAEGBU SHEDRACK IWUCHUkWU <shedrack3@gmail.com>
2018-10-12 03:42:58 +0100

    updated the jasmine test for the allFeeds initial entries in feedreader.js

--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [{
     name: 'Udacity Blog',
-    url: 'http://blog.udacit Udacity Blogy.com/feed'
+    url: 'http://blog.udacity.com/feed'
 }, {
     name: 'CSS Tricks',
-    url: 'http://feeds.feedb UdacityBlogurner.com/CssTricks'
+    url: 'http://feeds.feedburner.com/CssTricks'
 }, {
     name: 'HTML5 Rocks',
     url: 'http://feeds.feedburner.com/html5rocks'
